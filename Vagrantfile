57b973f6deabba2f4b25ed0de936c2a11c8110c1
Vlad <deadmeey@yandex.ru>
2018-09-02 23:26:23 +0300

    fix for nginx

--- Vagrantfile
+++ Vagrantfile
@@ -1,13 +1,24 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
+required_plugins = ["vagrant-hostmanager", "vagrant-vbguest"]
+required_plugins.each do |plugin|
+    if !Vagrant.has_plugin?(plugin) then
+        system "echo Not installed required plugin: #{plugin} ..."
+            system "vagrant plugin install #{plugin}"
+    end
+end
+
 Vagrant.configure("2") do |config|
     config.vm.box = "centos/7"
+
+    config.ssh.insert_key = false
 
     config.hostmanager.enabled = true
     config.hostmanager.manage_host = true
     #config.hostmanager.manage_guest = true
-    [5433, 3000].each do |port|
+
+    [5432, 3000].each do |port|
         config.vm.network "forwarded_port", guest: port, host: port
     end
 
@@ -16,9 +27,9 @@ Vagrant.configure("2") do |config|
         node.hostmanager.aliases = %w(io.drug.vm admin.drug.vm api.drug.vm)
     end
 
-    config.vm.synced_folder ".", "/home/vagrant/htdocs", mount_options: ["dmode=775", "fmode=664"]
+   config.vm.synced_folder ".", "/var/www", mount_options: ["dmode=775", "fmode=664"]
 
-   #config.vm.provision "shell", name: 'environment.sh', path: './vagrant/environment/environment.sh'
+   config.vm.provision "shell", name: 'environment.sh', path: './vagrant/environment/environment.sh'
    #config.vm.provision "shell", name: 'application.sh', path: './vagrant/environment/application.sh'
    #config.vm.provision "shell", name: 'node.sh', args: ['v10.6.0'], path: './vagrant/environment/node.sh'
    #config.vm.provision "shell", name: 'autostart.sh', path: './vagrant/environment/autostart.sh'
--- vagrant/environment/environment.sh
+++ vagrant/environment/environment.sh
@@ -5,7 +5,7 @@ yum -y update
 #install repos
 yum install -y epel-release
 rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-yum -y install https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-redhat96-9.6-3.noarch.rpm
+yum -y install https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-2.noarch.rpm
 
 #exclude default postgresql repos
 sed -i '/name=CentOS-$releasever - Base/a exclude=postgresql*' /etc/yum.repos.d/CentOS-Base.repo
@@ -15,47 +15,49 @@ sed -i '/name=CentOS-$releasever - Updates/a exclude=postgresql*' /etc/yum.repos
 yum -y install mc nginx redis memcached httpd-tools cronie
 yum -y install php72w-common php72w-devel php72w-cli php72w-fpm php72w-mbstring php72w-gd php72w-pecl-imagick
 yum -y install php72w-opcache php72w-sodium php72w-xml php72w-pgsql
-yum -y install postgresql96 postgresql96-server postgresql96-contrib
+yum -y install postgresql10 postgresql10-server postgresql10-contrib
 
 #init services
-/usr/pgsql-9.6/bin/postgresql96-setup initdb
+/usr/pgsql-10/bin/postgresql-10-setup initdb
 
 #stop services
 systemctl stop php-fpm
 systemctl stop nginx
 systemctl stop redis
 systemctl stop memcached
-systemctl stop postgresql-9.6
+systemctl stop postgresql-10
 
 #PHP configuration
 sed -i 's/^;date.timezone.*=.*$/date.timezone = UTC/' /etc/php.ini
 sed -i 's/^;upload_max_filesize.*=.*$/upload_max_filesiza=20M/' /etc/php.ini
 sed -i 's/^;post_max_size.*=.*/post_max_size=20M/' /etc/php.ini
 
-#disabled selinux for nginx, reload need
+#disabled selinux for nginx
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+setenforce 0
 
 #POSTGRESQL configuration
-sed -i "^#listen_addresses.*=.*$/listen_addresses.*= '*'/" /var/lib/pgsql/9.6/data/postgresql.conf
+sed -i "s/^#listen_addresses.*=.*$/listen_addresses = '*'/" /var/lib/pgsql/10/data/postgresql.conf
 
 #copy configs
-\cp /home/vagrant/htdocs/vagrant/postgresql/* /var/lib/pgsql/9.6/data
+\cp /home/vagrant/htdocs/vagrant/postgresql/* /var/lib/pgsql/10/data
 \cp /home/vagrant/htdocs/vagrant/php-fpm/* /etc/php-fpm.d
 \cp /home/vagrant/htdocs/vagrant/nginx/conf.d/* /etc/nginx/conf.d
+\cp /home/vagrant/htdocs/nginx.conf /etc/nginx
 
 #start services
 systemctl start php-fpm
 systemctl start nginx
 systemctl start redis
 systemctl start memcached
-systemctl start postgresql-9.6
+systemctl start postgresql-10
 
 #autostart services
 systemctl enable php-fpm
 systemctl enable nginx
 systemctl enable redis
 systemctl enable memcached
-systemctl enable postgresql-9.6
+systemctl enable postgresql-10
 
 #install composer
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
